e4826befb7696e8ae540cf7c070fa3d327d0f2f6
Igor Fedorov <iamfedoroff@yandex.ru>
2019-10-30 10:35:44 +0300

    Switched to yarn, add release script

---
+++ tasks/release.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+if [ ! $1 ]; then
+  echo "Error: you should pass version number"
+else
+  echo "[icons release]: creating version"
+  yarn version --no-git-tag-version --new-version $1
+
+  echo "[icons release]: build"
+  yarn run build
+
+  echo "[icons release]: commit"
+  git add -A && git commit -m "v$1"
+
+  echo "[icons release]: add tag"
+  git tag -a "v$1" -m "v$1"
+
+  echo "[icons release]: pushing updates"
+  git push origin master
+
+  echo "[icons release]: pushing new tag"
+  git push origin "v$1"
+
+  echo "[icons release]: publish to npm"
+  npm publish --access-public
+fi
