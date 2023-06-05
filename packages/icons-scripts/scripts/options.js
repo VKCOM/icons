b770349766fdac0d4f60d42098a2f146ccdf2cfb
Igor Fedorov <iamfedoroff@yandex.ru>
2023-06-05 17:32:04 +0800

    feat(icons-scripts): Add noEmit option

--- packages/icons-scripts/scripts/options.js
+++ packages/icons-scripts/scripts/options.js
@@ -14,6 +14,7 @@ const { debugError } = require('./utils');
  * @property {boolean} [keepTSSources]
  * @property {string[]} [extraCategories] List of glob patterns for directory with icons
  * @property {string} [cwd]
+ * @property {boolean} [noEmit]
  * @property {any[]} [svgoPlugins]
  * @property {Function} [onIconProcess]
  * @property {DeprecatedIcons} [deprecatedIcons] List of deprecated icons
@@ -31,6 +32,7 @@ function prepareOptions(options) {
     keepTSSources,
     extraCategories = ['Unsorted'],
     cwd = process.cwd(),
+    noEmit = false,
     svgoPlugins = [],
     onIconProcess = () => {},
     deprecatedIcons = {},
@@ -57,6 +59,7 @@ function prepareOptions(options) {
       : path.resolve(distDirectory, '../ts'),
     extraCategories,
     cwd,
+    noEmit,
     svgoPlugins,
     onIconProcess,
     deprecatedIcons,
