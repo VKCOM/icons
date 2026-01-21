e8a1e8b033ea12b8c0cfbac4234b08bb52a31c09
Daniil Suvorov <severecloud@gmail.com>
2026-01-21 10:52:36 +0300

    refactor: use biome Удаляем prettier, вместо него используем biome

--- packages/icons-sprite/src/index.ts
+++ packages/icons-sprite/src/index.ts
@@ -1,10 +1,10 @@
-export { SvgIconRoot, type SvgIconProps } from './SvgIconRoot';
-export { SvgIconRootV2 } from './SvgIconRootV2';
-export { makeRasterIcon } from './raster/components/RasterIcon/RasterIcon';
 export { IconSettingsProvider } from './IconSettings';
 export {
   appearanceTypes,
-  IconAppearanceProvider,
   type IconAppearance,
+  IconAppearanceProvider,
   useIconAppearance,
 } from './raster/components/IconAppearanceProvider/IconAppearanceProvider';
+export { makeRasterIcon } from './raster/components/RasterIcon/RasterIcon';
+export { type SvgIconProps, SvgIconRoot } from './SvgIconRoot';
+export { SvgIconRootV2 } from './SvgIconRootV2';
